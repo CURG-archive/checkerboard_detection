7cc4e98fc9f98ee2b2fc3c56ce1ab2fdacb848ce
jon-weisz <jon.weisz@gmail.com>
2013-08-29 14:33:48 -0400

    initial commit

---
+++ build/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/armuser/ros/rosbuild_src/checkerboard_detection/build/devel/setup.sh"
+emulate zsh # back to zsh mode
