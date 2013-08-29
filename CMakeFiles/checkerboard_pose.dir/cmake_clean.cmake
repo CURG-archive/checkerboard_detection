c19b9ce33e175fef0a528bdd10a60a2c54a1bb32
jon-weisz <jon.weisz@gmail.com>
2013-08-29 13:38:46 -0400

    initial commit

---
+++ CMakeFiles/checkerboard_pose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/checkerboard_pose.dir/src/estimate_pose.cpp.o"
+  "bin/checkerboard_pose.pdb"
+  "bin/checkerboard_pose"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/checkerboard_pose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/devel/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+CATKIN_SHELL=bash
+. "/home/armuser/ros/rosbuild_src/checkerboard_detection/build/devel/setup.sh"
